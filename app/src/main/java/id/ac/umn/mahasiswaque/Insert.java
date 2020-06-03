385ee0ed6cf17df2e55b1366d7a49beda5ac0a34
r00sha <r00sha@users.noreply.github.com>
2020-06-03 20:50:08 +0700

    banyak. pusing

--- app/src/main/java/id/ac/umn/mahasiswaque/Insert.java
+++ app/src/main/java/id/ac/umn/mahasiswaque/Insert.java
@@ -1,22 +1,74 @@
 package id.ac.umn.mahasiswaque;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class Insert extends AppCompatActivity {
+    ImageView profPic;
+    Button btnChange;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
 
+        profPic = (ImageView) findViewById(R.id.profPic);
+        btnChange = (Button)findViewById(R.id.btnChange) ;
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+//                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+//                builder.setTitle(R.string.app_name);
+//                builder.setMessage("Choose where to load");
+//                builder.setPositiveButton("Camera", new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        dialog.dismiss();
+//
+//                    }
+//
+//                });
+//                builder.setNegativeButton("Gallery", new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        dialog.dismiss();
+//                        openGallery();
+//                    }
+//                });
+//                AlertDialog alert = builder.create();
+//                alert.show();
+            }
+        });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            profPic.setImageURI(imageUri);
+        }
     }
 
     public void insData(View view) {
