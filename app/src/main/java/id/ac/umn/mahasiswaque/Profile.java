836cc27481db4776f84d5a9d2d500ecb4e4670db
r00sha <r00sha@users.noreply.github.com>
2020-06-03 21:52:28 +0700

    banyak. pusing

--- app/src/main/java/id/ac/umn/mahasiswaque/Profile.java
+++ app/src/main/java/id/ac/umn/mahasiswaque/Profile.java
@@ -2,16 +2,42 @@ package id.ac.umn.mahasiswaque;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class Profile extends AppCompatActivity {
+    private Switch swOnOff;
+    View bgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        swOnOff = (Switch)findViewById(R.id.swOnOff);
+        bgView = findViewById(R.id.bgView);
+
+        swOnOff.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if(swOnOff.isChecked()){
+                    swOnOff.setChecked(true);
+                    bgView.setAlpha(1f);
+                    Toast.makeText(getApplicationContext(),"Dark mode",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    swOnOff.setChecked(false);
+                    bgView.setAlpha(0f);
+                    Toast.makeText(getApplicationContext(),"Light mode",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
