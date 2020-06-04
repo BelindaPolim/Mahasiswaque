9c1e7fba0cdaa9cc7c4253987fd58c1454a5b87d
r00sha <r00sha@users.noreply.github.com>
2020-06-04 07:26:37 +0700

    gtw ah

--- app/src/main/java/id/ac/umn/mahasiswaque/HomeActivity.java
+++ app/src/main/java/id/ac/umn/mahasiswaque/HomeActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -16,13 +21,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.logging.Handler;
 import java.util.prefs.Preferences;
 
 public class HomeActivity extends AppCompatActivity {
     EditText inputSearch;
+    ListView listView;
+    FirebaseDatabase db;
+    DatabaseReference reff;
+    ArrayList<String> arrayList = new ArrayList<>();
     ArrayAdapter<String> adapter;
 
     @Override
@@ -39,27 +50,43 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-        inputSearch = (EditText) findViewById(R.id.inputSearch);
-        inputSearch.addTextChangedListener(new TextWatcher() {
+        db = FirebaseDatabase.getInstance();
+        reff = db.getReference("Users");
 
+        listView = (ListView) findViewById(R.id.lists);
+
+        Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show();
+        reff.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
-                // When user changed the Text
-                HomeActivity.this.adapter.getFilter().filter(cs);
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value=dataSnapshot.getValue(String.class);
+                arrayList.add(value);
+                adapter = new ArrayAdapter<String>(HomeActivity.this, android.R.layout.simple_list_item_1, arrayList);
+                listView.setAdapter(adapter);
+
             }
 
             @Override
-            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
-                                          int arg3) {
-                // TODO Auto-generated method stub
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
 
             }
 
             @Override
-            public void afterTextChanged(Editable arg0) {
-                // TODO Auto-generated method stub
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
+
     }
 
     @Override
@@ -72,26 +99,10 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.sortNIM:
-//                Collections.sort(arrayList);
-//                Collections.reverse(arrayList);
-//                for (int i = 0; i < arrayList.size(); i++) {
-//                    System.out.println(arrayList.get(i));
-//                }
-//                adapter = new ArrayAdapter<String>(this,
-//                        android.R.layout.simple_list_item_1, arrayList);
-//                listView.setAdapter(adapter);
-//                adapter.notifyDataSetChanged();
+                Toast.makeText(HomeActivity.this,"Feature Locked",Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.sortNama:
-//                Collections.sort(arrayList);
-//                Collections.reverse(arrayList);
-//                for (int i = 0; i < arrayList.size(); i++) {
-//                    System.out.println(arrayList.get(i));
-//                }
-//                adapter = new ArrayAdapter<String>(this,
-//                        android.R.layout.simple_list_item_1, arrayList);
-//                listView.setAdapter(adapter);
-//                adapter.notifyDataSetChanged();
+                Toast.makeText(HomeActivity.this,"Not available yet",Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.abtMe:
                 Intent intent = new Intent(HomeActivity.this, Profile.class);
